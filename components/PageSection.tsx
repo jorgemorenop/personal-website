fb8ddd2b4c805805032deaab55976c9749050d74
jorge <jorgemorenodev@gmail.com>
2022-06-24 16:49:38 +0200

    More responsive margins

--- components/PageSection.tsx
+++ components/PageSection.tsx
@@ -5,10 +5,10 @@ export default function PageSection({sectionName, sectionId, children}) {
     return (
         <Element name={sectionId}>
 
-            <div className="bg-white m-8 w-full shadow-md p-10">
+            <div className="bg-white my-8 w-full shadow-md p-10">
                 <h1>{sectionName}</h1>
                 <Divider variant="fullWidth"/>
-                <div className="mt-8">
+                <div className="mt-8 mb-5">
                     {children}
                 </div>
             </div>
