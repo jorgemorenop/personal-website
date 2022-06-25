9862960ab69b6b48651a53e1d6702aed77d70a8b
jorge <jorgemorenodev@gmail.com>
2022-06-25 13:57:13 +0200

    Better icon alignment

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -121,17 +121,17 @@ export default function Navbar() {
                                 <div className="grid grid-cols-2 gap-y-4 gap-x-8">
                                     {menuItems.map((x) => SectionLink({...x, isPopover: true}))}
                                 </div>
-                                <div className="grid grid-cols-8 gap-y-4 gap-x-2">
+                                <div className="grid grid-cols-8 gap-y-4 gap-x-8">
                                     <a href="https://github.com/jorgemorenop"
                                        className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
                                         <AiFillGithub size="3rem"/>
                                     </a>
                                     <a href="https://www.linkedin.com/in/jorgemorenop/"
-                                       className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900 ml-2">
+                                       className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
                                         <AiFillLinkedin size="3rem"/>
                                     </a>
                                     <a href="mailto:jorgemorenodev@gmail.com"
-                                       className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900 ml-2">
+                                       className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
                                         <AiOutlineMail size="3rem"/>
                                     </a>
                                 </div>
